2e2f29d68fd016763bfe13d510279500cb9fbaaa
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 12:49:25 +0530

    columnsToShow.json still working

---
+++ bin/v3/commands/initTableFromCdn/template/v4/Index/FormLoad/DomContentLoaded/BuildTable/getKSTableConfig.js
@@ -0,0 +1,21 @@
+import { getDefaultKSTableConfig } from "./getDefaultKSTableConfig.js";
+
+export const getKSTableConfig = async ({ tableName }) => {
+    const config = getDefaultKSTableConfig();
+
+    const fromPromise = await fetch("columnsToShow.json");
+    // debugger;
+    const columnsConfig = await fromPromise.json();
+
+    config.columnsConfig = columnsConfig[tableName].columnsConfig;
+
+    config.tableName = tableName;
+
+    config.endPoints.read = `/Api/V1/${tableName}/ShowAll`;
+    config.endPoints.filter = `/Api/V1/${tableName}/FilterColumns`;
+
+    config.endPoints.dataListEndpoints.AccountNames = `/Api/V1/${tableName}/Distinct/AccountName`;
+    config.endPoints.dataListEndpoints.Dates = `/Api/V1/${tableName}/Distinct/Date`;
+
+    return config;
+};
