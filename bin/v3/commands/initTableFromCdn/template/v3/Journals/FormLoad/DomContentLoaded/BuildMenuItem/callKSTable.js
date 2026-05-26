b074e949a591b949b91585bb2091cd08271dade9
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 11:48:28 +0530

    v3 added

---
+++ bin/v3/commands/initTableFromCdn/template/v3/Journals/FormLoad/DomContentLoaded/BuildMenuItem/callKSTable.js
@@ -0,0 +1,8 @@
+export const callKSTable = async (config) => {
+    if (window?.KSTable?.initShowTable) {
+        console.log("loaded from cdn");
+
+        return window.KSTable.initShowTable(config);
+    };
+    return false;
+};
